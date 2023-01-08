89bd78c8ecaf0adf6f1efc3a8304aea69dd5964b
Drizzle3 <93099842+Drizzle3@users.noreply.github.com>
2023-01-08 17:50:24 +0000

    Add files via upload

---
+++ assets/images/EquipPubData.js
@@ -0,0 +1,28 @@
+const EquipPubData = [
+    {
+        id: 'an-1',
+        user: "Evan Dandero",
+        description:
+        "Je suis à la recherche d'équipement de camping (tente, sac de couchage) a louer pour 4 jours.",
+    },
+    {
+        id: 'an-2',
+        user: "Ibrahim Nassir",
+        description:
+        "J’offre à location chargeur et adaptateur de voyage pour vos appareils électroniques. Pour plus de détails merci de me contacter.",
+    },
+    {
+        id: 'an-3',
+        user: "Samir Karti",
+        description:
+        "J’ai besoin d’une planche de surf pour une durée de 2 jours maximum.",
+    },
+    {
+        id: 'an-4',
+        user: "Darlene Steward",
+        description:
+        "J’ai besoin d’un sac à couchage pour une durée de 4 jours (à louer).",
+    },
+];
+
+export default EquipPubData; 
---
+++ assets/images/TripPubData.js
@@ -0,0 +1,40 @@
+const TripPubData = [
+    {
+      id: 'TripPub-1',
+      title: 'Trip To Safi',
+      image: require('../images/kayak.png'),
+      imageBig: require('../images/kayak-big.png'),
+      description:
+      "Bonjour, j aimerais bien trouver des accompagnants pour faire un trip à Safi pour plus de détails merci de m envoyer un message. "   , 
+      liked: false,
+      Depart: "Agadir",
+      destination: "Safi",
+      duration: 5/6,
+    },
+    {
+        id: 'TripPub-2',
+        title: 'Trip To Agadir',
+        image: require('../images/kayak.png'),
+        imageBig: require('../images/kayak-big.png'),
+        description:
+        "Hey, je souhaite trouver des compagnons pour faire un voyage à Agadir pour plus de détails veuillez m'envoyer un message. "   , 
+        liked: false,
+        Depart: "Essaouira",
+        destination: "Agadir",
+        duration: 2,
+      },
+      {
+        id: 'TripPub-3',
+        title: 'Trip To Tangier',
+        image: require('../images/kayak.png'),
+        imageBig: require('../images/kayak-big.png'),
+        description:
+        "Hello, i’m looking for persons to trip 4 days in Tangier"   , 
+        liked: false,
+        Depart: "Rabat",
+        destination: "Tanger",
+        duration: 4,
+      },
+  ];
+  
+  export default TripPubData;
